8abeb2bc8e191630daf22143fa8e7bd6e524ebf8
Jin Hyuk Park <llvslr77@gmail.com>
2020-09-24 23:38:29 +0000

    Automatically backed up by Learn

---
+++ case_statement.rb
@@ -0,0 +1,14 @@
+# name = "Alice"
+# case name
+# when "Alice"
+#   puts "Hello, Alice!"
+# when "The White Rabbit"
+#   puts "Don't be late, White Rabbit"
+# when "The mad Hatter"
+#   puts "Welcome to the tea party, Mad Hatter"
+# when "The Queen of hearts"
+#   puts "Please don't chop off my head"
+# else 
+#   puts "Whoooo are you?"
+# end
+
